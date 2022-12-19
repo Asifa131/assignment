e34ef51edc68d24066d968bb274a36979df8b602
Asifa131 <120708789+Asifa131@users.noreply.github.com>
2022-12-19 18:56:53 +0530

    changes

--- formrouter/src/form2.js
+++ formrouter/src/form2.js
@@ -39,35 +39,69 @@ class Form extends Component{
        if(typeof fields["email"] !== "undefined"){
          let lastAtPos =fields["email"].lastIndexOf("@");
          let lastDotPos =fields["email"].lastIndexOf(".");
+          if(
+            !(
+              lastAtPos < lastDotPos && 
+              lastAtPos > 0 &&
+              fields["email"].indexOf("@@") ==
+              lastDotPos > 2 &&
+              fields["email"].length - lastDotPos > 2
+            )
+          ) {
+            formIsValid =false;
+            errors["email"] = "Email is not valid";
+          }
        }
+       this.setState({errors:errors});
+       return formIsValid;
+      }
+      contactSubmit(e) {
+        e.preventDefault();
+        if ( this.handleValidation()){
+          alert("Form submitted");
+        } else {
+          alert("Form has errors");
+        }
+      }
+      handleChange(field , e) {
+        let fields = this.state.fields;
+        fields[field] = e.target.value;
+        this.setState({fields});
       }
   render(){
         return(
             <div>
-              <form className='frm'>
+              <form name="signupform" className='frm' onSubmit={this.contactSubmit.bind(this)} >
               <h2> Welcome To signup form </h2>
-            <br/>
-                <label>FullName</label><br/>
-                  <input type="text"name="FullName"/>
-                   <br/><br/>
 
-                   <label>Email</label><br/>
-                  <input type="email"name="email"/>
-                   <br/><br/>
+                 
+                  <input ref="name" type="text" size="30" placeholder="Name" 
+                     onChange={this.handleChange.bind(this,"name")}
+                     value={this.state.fields["name"]} />
+                   <br/> <br/>
 
-                   <label>Phone</label><br/>
-                   <input type="number"/>
-                   <br/><br/>
+                   <input ref="email" type="text" size="30" placeholder="Email" 
+                     onChange={this.handleChange.bind(this,"email")}
+                     value={this.state.fields["email"]} />
+                  <br/> <br/>
 
-                   <label>Password</label><br/>
-                   <input type="password"name="password"/>
-                   <br/><br/>
+                  <input ref="phone" type="text" size="30" placeholder="Phone" 
+                     onChange={this.handleChange.bind(this,"phone")}
+                     value={this.state.fields["phone"]} />
+                   <br/> <br/>
 
-                   <label>Confirm Password</label><br/>
-                   <input type="password"name="password"/>
+                   <input ref="password" type="text" size="30" placeholder="Password" 
+                     onChange={this.handleChange.bind(this,"password")}
+                     value={this.state.fields["password"]} />
+                   <br/> <br/>
+
+                   <input ref="confirmpassword" type="text" size="30" placeholder="Confirm Password" 
+                     onChange={this.handleChange.bind(this,"Confirmpassword")}
+                     value={this.state.fields["confirmpassword"]} />
                    <br/><br/>
 
                    <button  onClick={()=>alert("you have logged in")}>Submit</button> 
+                   
               </form>
             </div>
         )
--- login form/src/App.js
+++ login form/src/App.js
@@ -1,23 +1,80 @@
-import './App.css';
+import React from 'react';
+import {Formik , Form , Field , ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
+const LoginSchema = Yup.object().shape ( {
+    email: Yup.string()
+        .email("Invalid  email address format")
+        .required("Email is required") ,
+    password: Yup.string()
+        .min(3 , "Password must be 3 characters at minimum")    
+        .required("Password is required") ,
+
+});   
+
+
+class App extends React.Component {
+      render() {
   return (
-    <div className="container">
-      <div className='main'>
-        <h1 className='header'>LOGIN FORM</h1>
-          <div className='sub-main'>USERNAME:<br></br>
-             <input type="text"className="element"placeholder="Name"/>
+      <div className='container'>
+          <div className='row'>
+            <div className='col-lg-12'> 
+  <Formik Formik 
+       initialValues = {{ email:" ",  password:" "}}
+       validationSchema = {LoginSchema}  
+       onSubmit = {(values) =>{
+         console.log(values);
+         alert("Form is validated! Submitting the form...")
+       }}
+       >
+        {({ touched , errors , isSubmitting , values}) =>
+         !isSubmitting ? (
+           <div>
+            <div className='row mb-5'>
+              <div className='col-lg-12 text-center'> 
+               <h1 className='mt-5'>LOGIN FORM</h1>
+           </div>
+            </div>
+              <Form> 
+                 <div className="form-group">
+                   <label htmlFor='email'>Email</label>
+                    <Field  type="email" name="email" placeholder="Enter email"
+                     className={` mt-2 form-control ${ touched.email && errors.email ? "is-invalid" : " "}`} />
+
+                  <ErrorMessage component="div" name="email" className="invalid-feedback" />
+                 </div>
+
+                  <div className="form-group">
+                   <label htmlFor='password'className='mt-3'>Password</label>
+                    <Field  type="password" name="password" placeholder="Enter password"
+                     className={` mt-2 form-control ${ touched.password && errors.password ? "is-invalid" : " "}`} />
+
+                  <ErrorMessage component="div" name="password" className="invalid-feedback" />
+                  </div>
+            <button type='submit' className='btn btn-primary btn-block mt-4'> Submit  </button>
+           </Form>
+      </div>
+    ) : (
+        <div>
+          <h1 className='p-3 mt-5'>Form submitted</h1>
+
+          <div className='alert alert-success mt-3'>
+              Thankyou for your connecting with us. Here's what we got from you !
           </div>
-             <br></br>
-         <div className='sub-main'>PASSWORD:<br></br>
-            <input type="text"className="element"placeholder="Password"/>
-            <br></br>
-            <br></br>
-            <button onClick={()=>alert("you have logged in")} className='btn'>Submit</button>
-         </div>
+          <ul className='list-group'>
+            <li className='list-group-item'>Email: {values.email}</li>
+            <li className='list-group-item'>Password: {values.password}</li>
+          </ul>
+        </div>
+    )
+        }
+        </Formik>
+        </div>
       </div>
     </div>
   );
+    }
 }
 
 export default App;
